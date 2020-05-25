a296fde6f6772a2a8a756eecf9d9aab9bb145903
Jakub Doczy <423199@mail.muni.cz>
2020-05-25 13:09:50 +0200

    Split moved parsing of ProgramOptions to inner class

--- src/main/java/ProgramOptions.java
+++ src/main/java/ProgramOptions.java
@@ -6,8 +6,6 @@ import java.util.*;
 
 /**
  * Global options of the program.
- *
- * TODO: split this class into parser and data holder
  */
 public class ProgramOptions {
 
@@ -18,57 +16,17 @@ public class ProgramOptions {
         MANIPULATION_METHODS("-m"),
         OUTPUT_TYPE("-o");
 
-        private CommandLineOptions(String str) {
+        CommandLineOptions(String str) {
             this.str = str;
         }
 
         public String str;
     }
 
-    private int parsedIndex;
-    private String[] args;
-
     private String outputPath;
     private String inputPath;
     private List<String> manipulationMethods;
     private OutputType outputType;
-
-    public ProgramOptions(String[] commandLineArgs) {
-        parsedIndex = 0;
-        args = commandLineArgs;
-        manipulationMethods = new ArrayList<>();
-        parseArgs();
-    }
-
-    private Map<String, Runnable> createParseMap() {
-        Map<String, Runnable> parseMap = new HashMap<>();
-        parseMap.put(CommandLineOptions.DATASET_LOCATION.str, new Runnable() {
-            @Override
-            public void run() {
-                log.debug("Input URL: " + args[parsedIndex]);
-                inputPath = args[parsedIndex];
-                parsedIndex++;
-            }
-        });
-        parseMap.put(CommandLineOptions.MANIPULATION_METHODS.str, new Runnable() {
-            @Override
-            public void run() {
-                log.debug("Parsing manipulation methods from index " + parsedIndex);
-                parseManipulationMethods();
-            }
-        });
-        parseMap.put(CommandLineOptions.OUTPUT_TYPE.str, new Runnable() {
-            @Override
-            public void run() {
-                outputType = OutputType.parse(args[parsedIndex++]);
-                log.debug("Output type: " + outputType.str);
-                outputPath = args[parsedIndex++];
-                log.debug("Output path: " + outputPath);
-            }
-        });
-
-        return parseMap;
-    }
 
     public String getOutputPath() {
         return outputPath;
@@ -86,33 +44,74 @@ public class ProgramOptions {
         return outputType;
     }
 
-    private void parseManipulationMethods() {
-        if (args[parsedIndex].isEmpty() || args[parsedIndex].startsWith("-")) {
-            log.error("Found no data manipulation methods.");
-            throw new RuntimeException("No manipulation methods selected.");
-        }
-
-        while (parsedIndex < args.length && !args[parsedIndex].startsWith("-")) {
-            manipulationMethods.add(args[parsedIndex]);
-            parsedIndex++;
-        }
+    public ProgramOptions(String[] commandLineArgs) {
+        manipulationMethods = new ArrayList<>();
+        new ProgramOptionsParser(commandLineArgs);
+        log.debug("Successfully parsed all arguments.");
     }
 
-    private void parseArgs() {
-        Map<String, Runnable> parseMap = createParseMap();
-        log.debug("Parsing arguments " + Arrays.toString(args));
+    private class ProgramOptionsParser {
+        private int parsedIndex;
+        private String[] args;
 
-        while (args.length > parsedIndex) {
-            log.debug("Parsing argument " + args[parsedIndex]);
-            if (parseMap.containsKey(args[parsedIndex])) {
-                parseMap.get(args[parsedIndex++]).run();
-            } else {
-                log.error("Failed to parse argument " + args[parsedIndex] + ", Expected one of options.");
-                throw new RuntimeException("Unrecognised option: " + args[parsedIndex]);
+        ProgramOptionsParser(String[] commandLineArgs) {
+            parsedIndex = 0;
+            args = commandLineArgs;
+            parseArgs();
+        }
+
+        private Map<String, Runnable> createParseMap() {
+            Map<String, Runnable> parseMap = new HashMap<>();
+            parseMap.put(CommandLineOptions.DATASET_LOCATION.str, () ->
+                {
+                    log.debug("Input URL: " + args[parsedIndex]);
+                    inputPath = args[parsedIndex];
+                    parsedIndex++;
+                });
+            parseMap.put(CommandLineOptions.MANIPULATION_METHODS.str, () ->
+                {
+                    log.debug("Parsing manipulation methods from index " + parsedIndex);
+                    parseManipulationMethods();
+                });
+            parseMap.put(CommandLineOptions.OUTPUT_TYPE.str, () ->
+                {
+                    outputType = OutputType.parse(args[parsedIndex++]);
+                    log.debug("Output type: " + outputType.str);
+                    outputPath = args[parsedIndex++];
+                    log.debug("Output path: " + outputPath);
+                });
+
+            return parseMap;
+        }
+
+        private void parseManipulationMethods() {
+            if (args[parsedIndex].isEmpty() || args[parsedIndex].startsWith("-")) {
+                log.error("Found no data manipulation methods.");
+                throw new RuntimeException("No manipulation methods selected.");
+            }
+
+            while (parsedIndex < args.length && !args[parsedIndex].startsWith("-")) {
+                manipulationMethods.add(args[parsedIndex]);
+                parsedIndex++;
             }
         }
 
-        log.debug("Successfully parsed all arguments.");
+        private void parseArgs() {
+            Map<String, Runnable> parseMap = createParseMap();
+            log.debug("Parsing arguments " + Arrays.toString(args));
+
+            while (args.length > parsedIndex) {
+                log.debug("Parsing argument " + args[parsedIndex]);
+                if (parseMap.containsKey(args[parsedIndex])) {
+                    parseMap.get(args[parsedIndex++]).run();
+                } else {
+                    log.error("Failed to parse argument " + args[parsedIndex] + ", Expected one of options.");
+                    throw new RuntimeException("Unrecognised option: " + args[parsedIndex]);
+                }
+            }
+        }
+
     }
+
 
 }
